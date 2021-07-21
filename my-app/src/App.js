dc3404ba563ccdc04df5d6b3254f6201be57815b
MorningClub <35296910+MorningClub@users.noreply.github.com>
2021-07-21 10:12:14 +0200

    uhi

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,48 +12,13 @@ import {
 } from 'recoil';
 
 
-
-// const page = atom({
-//   key: "page",
-//   default: 0
-// });
-
-
-
 function App() {
 
 
   return (
       <MainPage />
     );
-  
-  // return (
-  //   <RecoilRoot>
-  //     <Router />  
-  //   </RecoilRoot>
-  // );
- 
 }
 
-// function Router() {
-//   const [state, setState] = useRecoilState(page);
-  
-//   switch(state){
-//     case 0 :
-//       return (
-//         <Login />
-//       );
-//     case 1 :
-//       return <MainPage />
-//     default:
-//       return (
-//         <div className="App">
-        
-//           <button onClick={() => setState(1)}>Logg inn</button>
-//         </div>
-//       );
-//   }
- 
-// }
 
 export default App;
