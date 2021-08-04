ece40be8b1560a3a1172fc328210200dc1c4b0ee
askattilla <mail@askattilla.com>
2021-08-04 09:08:39 +0200

    merget med barnsiden

--- src/App.js
+++ src/App.js
@@ -27,8 +27,7 @@ import Portal from './pages/Portal/Portal';
 
 const page = atom({
   key: "page",
-  // default: PAGE_POINTER.login
-  default: PAGE_POINTER.situation
+  default: PAGE_POINTER.login
 
 });
 const lastPage = atom({
